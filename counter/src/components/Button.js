492e8a7086e8717e436f6293e2465d4ce8d607ff
drignet <odibehenry@gmail.com>
2022-11-23 04:28:50 +0100

    my react codes

--- counter/src/components/Button.js
+++ counter/src/components/Button.js
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react'
+import {useState} from 'react'
 
 const Button = (props) => {
   const [count , setCount] = useState(0)
@@ -7,7 +7,7 @@ const Button = (props) => {
   const next = () => setCount(count + 1)
   return (
     <div className='text-white bg-gray-700 shadow-xl rounded-lg'>
-      <p className='text-9xl text-center p-2' style={count < 0 ? {color: 'red'} : count == 0 ? {color: 'white'} : {color: 'green'}}>{count}</p>
+      <p className='text-9xl text-center p-2' style={count < 0 ? {color: 'red'} : count === 0 ? {color: 'white'} : {color: 'green'}}>{count}</p>
       <div className='flex gap-4 bg-green-500 p-4 rounded-b-lg'>
         <button onClick={prev}><img src={props.prev} alt='prev' width='80' height='80' className='shadow-xl rounded-full' /></button>
         <button onClick={reset}><img src={props.reset} alt='reset' width='80' height='80' className='shadow-xl rounded-full border-2 border-red-900' /></button>
